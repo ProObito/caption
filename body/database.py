8c6eba8af827d12b1b815d9465ea4569b5c5743b
ProObito <ahmedumaid707@gmail.com>
2025-05-17 19:27:16 +0530

    Update database.py

--- body/database.py
+++ body/database.py
@@ -6,13 +6,18 @@ db = client.captions_with_chnl
 chnl_ids = db.chnl_ids
 users = db.users
 
-async def addCap(chnl_id, caption):
-    dets = {"chnl_id": chnl_id, "caption": caption}
+async def addCap(chnl_id, caption, font_style="NONE"):
+    dets = {"chnl_id": chnl_id, "caption": caption, "font_style": font_style}
     await chnl_ids.insert_one(dets)
 
+async def updateCap(chnl_id, caption, font_style=None):
+    update_dict = {"caption": caption}
+    if font_style is not None:
+        update_dict["font_style"] = font_style
+    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": update_dict})
 
-async def updateCap(chnl_id, caption):
-    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
+async def updateFontStyle(chnl_id, font_style):
+    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"font_style": font_style}})
 
 async def insert(user_id):
     user_det = {"_id": user_id}
