ac22e8aa21dff75e3075f162f3c68a20cbe19d60
ProObito <ahmedumaid707@gmail.com>
2025-05-18 19:56:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ API_HASH = str(getenv("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7976984538:AAGLp9tX4gD3oepR40KtD6ANwYH-Hy6GPOY"))
 FORCE_SUB_1 = "-1001234567890"  # First channel ID (update with valid ID)
 FORCE_SUB_2 = "-1002078429106"  # Second channel ID (update with valid ID)
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1002341265837") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://spxsolo:umaid2008@cluster0.7fbux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 START_GIF = "CAACAgUAAxkBAAEFBAVoH4qTFGwjwrCkLJPeM0HjglJpYgACXAgAArSfGVXK3kCuYAiK2B4E"  # Replace with your GIF/video file_id or URL
 START_REACTION = "🔥"  # Reaction emoji for /start message
