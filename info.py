c83837a3cfd0f3b9d10f7d1ab689ee3f075e4f35
ProObito <ahmedumaid707@gmail.com>
2025-05-18 22:22:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,6 +21,7 @@ API_HASH = str(getenv("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7976984538:AAGLp9tX4gD3oepR40KtD6ANwYH-Hy6GPOY"))
 FORCE_SUB_1 = "-1001234567890"  # First channel ID (update with valid ID)
 FORCE_SUB_2 = "-1002078429106"  # Second channel ID (update with valid ID)
+FORCE_SUB = os.environ.get("FORCE_SUB", "-1001919499026") 
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://spxsolo:umaid2008@cluster0.7fbux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 DEF_CAP = str(
     getenv(
